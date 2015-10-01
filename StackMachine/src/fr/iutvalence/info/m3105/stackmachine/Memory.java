e6b76b901bafabe91e10a24c9f77e283ecf8169d
loewb <benoit.loew@iut-valence.fr>
2015-10-01 09:33:17 +0200

    creating of some classes in code and VP

---
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Memory.java
@@ -0,0 +1,19 @@
+package fr.iutvalence.info.m3105.stackmachine;
+
+public class Memory {
+
+	public Memory(int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void write(int currentAddress, int instructionParam) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getStartAddress() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
